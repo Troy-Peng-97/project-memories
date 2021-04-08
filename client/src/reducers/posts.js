d391838dfd59770be6bf79b19e4e94774050528c
Qiankun Peng <qiankunpeng@QiankundeMacBook-Pro.local>
2021-04-07 19:31:11 -0700

    finish post card create and update functionality

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,19 @@
-// a reducer is a function
-// combined this function with the reducer;
-
-import Posts from "../components/Posts/Posts";
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
 export default (posts = [], action) => {
-    switch (action.type){
-        case 'FETCH_ALL':
-            return action.payload;
-        case 'CREATE':
-            return [...posts, action.payload];
-        default:
-            return Posts;
-    }
-}
+  switch (action.type) {
+    case FETCH_ALL:
+      return action.payload;
+    case LIKE:
+      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+    case CREATE:
+      return [...posts, action.payload];
+    case UPDATE:
+      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+    case DELETE:
+      return posts.filter((post) => post._id !== action.payload);
+    default:
+      return posts;
+  }
+};
+
